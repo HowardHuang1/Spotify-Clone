039be7f0bb5924ca822e4263f6563060f7ddd70e
Howard Huang <howardhuang2004@gmail.com>
2023-03-27 18:33:00 -0700

    parse user access token

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from "react";
 import './App.css';
 import Login from "./Login";
 import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./Player";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null);
@@ -12,7 +16,13 @@ function App() {
     const _token = hash.access_token;
 
     if (_token) {
-      setToken(_token)
+      setToken(_token);
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then((user) => {
+        console.log("User: " + user);
+      });
     }
 
     console.log('I HAVE A TOKEN >>> ', token);
@@ -21,11 +31,8 @@ function App() {
   return (
     <div className="App">
       {
-        token ? (
-          <h1>I am logged in</h1>
-        ) : (
-          <Login />
-        )
+        token ? 
+          <Player /> : <Login />
       }
     </div>
   );
