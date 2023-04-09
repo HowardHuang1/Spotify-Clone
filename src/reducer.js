eb9a51731795ebeb0f9323faf72d400497601bd0
Howard Huang <howardhuang2004@gmail.com>
2023-04-09 14:00:03 -0700

    completed footer

--- src/reducer.js
+++ src/reducer.js
@@ -3,7 +3,7 @@ export const initialState = {
     playlists: [],
     playing: false,
     item: null,
-    // token: "BQBbrccGi8RWvhCavKk-u3yMy2qwbMwtkEmAXBC6eLpLd7ZQF8xOBTB51be73tEzq5Zl1QlqWKgnR-JgwUDGMqof9odr91Hicif004LfnmpIrhZC1oOTgpkjTBXlmI-fEeIm5Le1qYfVK-HA0dVIJFWTiwsCFub-2xCXkPrmIcsS_V9obl3BrKJ5Im3VbCFeHFqy8rZUoFmcSRlpYZ6s4A",
+    token: "BQBbrccGi8RWvhCavKk-u3yMy2qwbMwtkEmAXBC6eLpLd7ZQF8xOBTB51be73tEzq5Zl1QlqWKgnR-JgwUDGMqof9odr91Hicif004LfnmpIrhZC1oOTgpkjTBXlmI-fEeIm5Le1qYfVK-HA0dVIJFWTiwsCFub-2xCXkPrmIcsS_V9obl3BrKJ5Im3VbCFeHFqy8rZUoFmcSRlpYZ6s4A",
 };
 
 const reducer = (state, action) => {
